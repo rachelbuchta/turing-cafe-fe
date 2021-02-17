c22b69bbd41380c9feb39c17d99f95e3e87a8c4d
Rachel Buchta <rachelbuchta@yahoo.com>
2021-02-17 10:07:56 -0700

    Create form component and import into app

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import { getReservations } from '../../util'
 import Reservations from '../Reservations/Reservations'
+import Form from '../Form/Form'
 
 class App extends Component {
   constructor() {
@@ -38,7 +39,7 @@ class App extends Component {
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          < Form />
         </div>
         <div className='resy-container'>
           <Reservations reservations={this.state.reservations}/>
---
+++ src/Components/Form/Form.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import './Form.css'
+
+class Form extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+
+
+  render() {
+    return (
+      <h1>Form</h1>
+    )
+  }
+}
+
+export default Form
+
