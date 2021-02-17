419d8e0001435d97472890051d9ca618f1decaba
Rachel Buchta <rachelbuchta@yahoo.com>
2021-02-17 10:17:07 -0700

    Add controlled form to make new reservation with date time name and guests

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -5,15 +5,51 @@ class Form extends Component {
   constructor() {
     super()
     this.state = {
-
+        name: '',
+        date: '',
+        time: '',
+        number: ''
     }
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
   }
 
 
 
   render() {
     return (
-      <h1>Form</h1>
+      <form>
+        <input
+          type='text'
+          name='name'
+          placeholder='Name'
+          value={this.state.name}
+          onChange={this.handleChange}
+        />
+        <input
+          type='date'
+          name='date'
+          value={this.state.date}
+          onChange={this.handleChange}
+        />
+        <input
+          type='time'
+          name='time'
+          value={this.state.time}
+          onChange={this.handleChange}
+        />
+        <input
+          type='number'
+          name='number'
+          placeholder='Number Of Guests'
+          value={this.state.number}
+          onChange={this.handleChange}
+        />
+      </form>
     )
   }
 }
